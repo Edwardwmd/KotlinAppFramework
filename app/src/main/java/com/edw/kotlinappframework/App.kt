2078889eb52de5c62e5a0e95f43e0bb5542b70bb
Edwardwmd <1732141816wmd@gmail.com>
2021-03-01 00:24:52 +0800

    添加koin bind的使用

--- app/src/main/java/com/edw/kotlinappframework/App.kt
+++ app/src/main/java/com/edw/kotlinappframework/App.kt
@@ -2,15 +2,10 @@ package com.edw.kotlinappframework
 
 import android.app.Application
 import com.alibaba.android.arouter.launcher.ARouter
-
-import com.edw.kotlinappframework.bean.KoinStudyBeanA
-import com.edw.kotlinappframework.bean.KoinStudyBeanB
-import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import dagger.hilt.android.HiltAndroidApp
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.KoinApplication
 import org.koin.core.context.startKoin
-import org.koin.core.logger.Logger
-import org.koin.dsl.module
 
 /**
  * Author: EdwardWMD
@@ -19,30 +14,27 @@ import org.koin.dsl.module
  * Website: https://github.com/Edwardwmd
  * Desc: File Information!
  */
+@HiltAndroidApp
 class App : Application() {
     private val isDebug: Boolean = true
+    private var koinApplication: KoinApplication? = null
 
     override fun onCreate() {
         super.onCreate()
         //1.初始化ARouter
         initARouter()
-
         //初始化Koin依赖注入框架
         initKoin()
-
-
     }
 
     private fun initKoin() {
-        startKoin {
+        koinApplication = startKoin {
             androidContext(this@App)
             modules(KoinModuleManager.instance.appMode)
-//            logger()
         }
-
     }
 
-    fun initARouter() {
+    private  fun initARouter() {
         if (isDebug) {           // 这两行必须写在init之前，否则这些配置在init过程中将无效
             ARouter.openLog()     // 打印日志
             ARouter.openDebug()   // 开启调试模式(如果在InstantRun模式下运行，必须开启调试模式！线上版本需要关闭,否则有安全风险)
@@ -55,5 +47,9 @@ class App : Application() {
         super.onTerminate()
         //销毁ARouter
         ARouter.getInstance().destroy()
+        //关闭Koin
+        koinApplication?.apply {
+            close()
+        }
     }
 }
